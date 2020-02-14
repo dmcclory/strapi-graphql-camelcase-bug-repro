e92ffee0d65aaf0f4b48ce16fbd1cc857b25796a
Dan McClory <danmcclory@gmail.com>
2020-02-14 10:01:24 -0500

    console.log some debug info in the policy

--- config/policies/ownershipPolicy.js
+++ config/policies/ownershipPolicy.js
@@ -3,6 +3,8 @@
 module.exports = async (ctx, next) => {
 
   console.log("I am a global policy and I am running!")
-
+  console.log(Object.keys(ctx.request.body))
+  console.log(ctx.request.body.operationName)
+  console.log(ctx.request.body.title)
   return await next()
 }
